4f04017c55ec03f052018babfa9e4770ae59cdf9
Avinash <avinash80501@gmail.com>
2025-08-11 23:03:44 +0530

    "Switched to torch"

--- decoder.py
+++ decoder.py
@@ -1,68 +1,55 @@
-from tensorflow.keras.layers import Embedding,Dense,Layer,Dropout
-from tensorflow import matmul,nn,reshape,shape,transpose,float32,cast,convert_to_tensor,Variable,reduce_mean,ones
-from tensorflow.linalg import band_part
-from tensorflow.math import reduce_std,sqrt
 import numpy as np
-from keras.saving import register_keras_serializable
+from torch import nn,triu,ones,float32,matmul,arange,sin,cos,zeros,mean,std,reshape,transpose
+import math
 
-    
-
-    
 def mask_input(X):
-    neg_inf=-1e-9
-    seq_len=shape(X)[-1]
-    mask=band_part(ones((seq_len,seq_len)),-1,0)
-    mask=1.0-mask
+    neg_inf=-1e9
+    seq_len=X.shape[-1]
+    mask=ones((seq_len,seq_len))
+    mask=triu(mask,1)
     mask=mask*neg_inf
-
-
-    return cast(mask,dtype=float32)
-
+    out=X+mask
+    return out
 #Positional embedding for the model to understand the positions obviously
-@register_keras_serializable()
-class PositionalEncoding(Layer):
+class PositionalEncoding(nn.Module):
     def __init__(self, vocab, d_model, max_len, **kwargs):
         super(PositionalEncoding, self).__init__(**kwargs)
         self.vocab=vocab
         self.d_model=d_model
         self.max_len=max_len
-        self.embedding=Embedding(input_dim=vocab,output_dim=d_model,input_length=max_len)
-        self.position_embedding=[]
-        
-        for pos in range(max_len):
-            embeddings=[]
-            for i in range(d_model):
-                if i%2 ==0:
-                    embeddings.append(np.cos(pos/10000**((2*i)/d_model)))
-                else:
-                    embeddings.append(np.sin(pos/10000**((2*i)/d_model)))
-            self.position_embedding.append(embeddings)
-        self.position_embedding=convert_to_tensor([self.position_embedding],dtype='float32')    
-        
-        
-    def call(self,X):
+        self.embedding=nn.Embedding(vocab,d_model)
+        pos = arange(max_len).unsqueeze(1)
+        i = arange(d_model).unsqueeze(0)
+        angle_rates = 1 / pow(10000, (2 * (i//2)) / d_model)
+
+        self.pe = zeros(max_len, d_model)
+        self.pe[:, 0::2] = sin(pos * angle_rates[:, 0::2])
+        self.pe[:, 1::2] = cos(pos * angle_rates[:, 1::2])
+
+        self.register_buffer("position_embedding", self.pe.unsqueeze(0))
+
+    def forward(self,X):
+        seq_len=X.shape[-1]
         X=self.embedding(X)
-        return X+self.position_embedding  
-        
-@register_keras_serializable()
-class MaskedMultiHeadSelfAttention(Layer):
+        return X+self.pe[:seq_len,:]
+class MaskedMultiHeadSelfAttention(nn.Module):
     def __init__(self,d_model,max_len,num_heads, **kwargs):
         super(MaskedMultiHeadSelfAttention,self).__init__( **kwargs)
         self.d_model=d_model
         self.max_len=max_len
         self.num_heads=num_heads
-        self.q=Dense(d_model)
-        self.k=Dense(d_model)
-        self.v=Dense(d_model)
-        self.final=Dense(d_model)
+        self.q=nn.Linear(d_model,d_model)
+        self.k=nn.Linear(d_model,d_model)
+        self.v=nn.Linear(d_model,d_model)
+        self.final=nn.Linear(d_model,d_model)
         
     def split_heads(self,x, num_heads,batch_size):
     # (batch, seq_len, d_model) → (batch, num_heads, seq_len, head_dim)
         
-        x = reshape(x, (batch_size, -1, num_heads, self.d_model // num_heads))
-        return transpose(x, perm=[0, 2, 1, 3])
-    def call(self,X):
-        batch_size=shape(X)[0]
+        x = x.reshape(batch_size, self.max_len, num_heads, self.d_model // num_heads)
+        return x.permute(0,2,1,3)
+    def forward(self,X):
+        batch_size=X.shape[0]
 
         q=self.q(X)
         k=self.k(X)
@@ -73,64 +60,63 @@ class MaskedMultiHeadSelfAttention(Layer):
         v=self.split_heads(v,self.num_heads,batch_size)
 
         #Scaled dot product
-        dk=cast(self.d_model//self.num_heads,float32)
-        at_score = matmul(q, k, transpose_b=True) / sqrt(cast(dk, float32))        
+        dk=self.d_model//self.num_heads
+        at_score = matmul( q, transpose(k,-2,-1)) / math.sqrt(dk)
         mask=mask_input(at_score)
-        weights=nn.softmax(at_score+mask,axis=-1)
+        weights=nn.functional.softmax(mask,-1)
 
-        attention=matmul(weights,v)
+        attention = matmul(weights, v)
 
         #merging 
-        attention=transpose(attention,perm=[0,2,1,3])#(batch_size,seq_len,heads,d_model)
-        concat_attention=reshape(attention,(batch_size,-1,self.d_model))
+        attention=attention.permute(0,2,1,3)#(batch_size,seq_len,heads,d_model)
+        concat_attention=attention.reshape(batch_size,-1,self.d_model)
         #to capture the final meaning of the word according to the context
         out=self.final(concat_attention)
         return out
     
-@register_keras_serializable()
-class FeedForward(Layer):
+class FeedForward(nn.Module):
     def __init__(self,units,d_model, **kwargs):
         super(FeedForward,self).__init__( **kwargs)
-        self.inputs=Dense(units,activation='relu')
-        self.out=Dense(d_model,activation='linear')
-    def call(self,X):
+        self.inputs=nn.Linear(d_model,units,)
+        self.out=nn.Linear(units,d_model)
+    def forward(self,X):
         X=self.inputs(X)  
+        X=nn.ReLU(X)
         X=self.out(X)
         return X
 
-@register_keras_serializable()
-class LayerNormalization(Layer):
+class LayerNormalization(nn.Module):
     def __init__(self,d_model,epsilon=1e-5, **kwargs):
         super(LayerNormalization,self).__init__( **kwargs)
         self.d_model=d_model
         self.epsilon=epsilon
-        self.beta=self.add_weight(name="beta",shape=(d_model,),initializer='zeros',trainable=True) 
-        self.gamma=self.add_weight(name="gamma",shape=(d_model,),initializer='ones',trainable=True)
-    def call(self,X):
-        mean = reduce_mean(X,axis=-1,keepdims=True)
-        std= reduce_std(X,axis=-1,keepdims=True)
-        X_norm=(X-mean)/(std+self.epsilon)
+        self.beta=nn.Parameter(zeros(d_model)) 
+        self.gamma=nn.Parameter(ones(d_model))
+    def forward(self,X):
+        mn = mean(X,-1,True)
+        stdiv= std(X,-1,keepdim=True)
+        X_norm=(X-mn)/(stdiv+self.epsilon)
         out=X_norm*self.gamma+self.beta
         return out
-@register_keras_serializable()
-class MultiHeadCrossAttention(Layer):
+
+class MultiHeadCrossAttention(nn.Module):
     def __init__(self,d_model,max_len,num_heads, **kwargs):
         super(MultiHeadCrossAttention,self).__init__( **kwargs)
         self.d_model=d_model
         self.max_len=max_len
         self.num_heads=num_heads
-        self.q=Dense(d_model)
-        self.k=Dense(d_model)
-        self.v=Dense(d_model)
-        self.final=Dense(d_model)
+        self.q=nn.Linear(d_model,d_model)
+        self.k=nn.Linear(d_model,d_model)
+        self.v=nn.Linear(d_model,d_model)
+        self.final=nn.Linear(d_model,d_model)
         
     def split_heads(self,x, num_heads,batch_size):
     # (batch, seq_len, d_model) → (batch, num_heads, seq_len, head_dim)
         
         x = reshape(x, (batch_size, -1, num_heads, self.d_model // num_heads))
-        return transpose(x, perm=[0, 2, 1, 3])
+        return x.permute(0, 2, 1, 3)
     def call(self,X,y):
-        batch_size=shape(X)[0]
+        batch_size=X.shape[0]
 
         q=self.q(y)
         k=self.k(X)
@@ -141,21 +127,20 @@ class MultiHeadCrossAttention(Layer):
         v=self.split_heads(v,self.num_heads,batch_size)
 
         #Scaled dot product
-        dk=cast(self.d_model//self.num_heads,float32)
-        at_score = matmul(q, k, transpose_b=True) / sqrt(cast(dk, float32))        
-        weights=nn.softmax(at_score,axis=-1)
+        dk=self.d_model//self.num_heads
+        at_score = matmul(q, transpose(k,-2,-1)) / math.sqrt(dk)        
+        weights=nn.functional.softmax(at_score,-1)
         attention=matmul(weights,v)
 
         #merging 
-        attention=transpose(attention,perm=[0,2,1,3])#(batch_size,seq_len,heads,d_model)
+        attention=attention.permute(0,2,1,3)#(batch_size,seq_len,heads,d_model)
         concat_attention=reshape(attention,(batch_size,-1,self.d_model))
         #to capture the final meaning of the word according to the context
         out=self.final(concat_attention)
         return out
     
 
-@register_keras_serializable
-class DecoderLayer(Layer):
+class DecoderLayer(nn.Module):
     def __init__(self,d_model,num_heads,max_len,ff_hidden,drop_prob,**kwargs):
         super(DecoderLayer,self).__init__(**kwargs)
         self.masked_attention=MaskedMultiHeadSelfAttention(d_model,max_len,num_heads)
@@ -164,10 +149,10 @@ class DecoderLayer(Layer):
         self.layer_Norm1=LayerNormalization(d_model)
         self.layer_Norm2=LayerNormalization(d_model)
         self.layer_Norm3=LayerNormalization(d_model)
-        self.dropout1=Dropout(drop_prob)
-        self.dropout2=Dropout(drop_prob)
-        self.dropout3=Dropout(drop_prob)
-    def call(self,X,y):
+        self.dropout1=nn.Dropout(drop_prob)
+        self.dropout2=nn.Dropout(drop_prob)
+        self.dropout3=nn.Dropout(drop_prob)
+    def forward(self,X,y):
         res=y
         out=self.masked_attention(y)
         out=self.dropout1(out)
